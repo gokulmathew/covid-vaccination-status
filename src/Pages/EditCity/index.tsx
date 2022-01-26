7464494f560bba3ff2b7d5155c714f0099668d14
gokulmathew <gokul.srinivasan22@gmail.com>
2022-01-26 15:39:31 -0500

    1

--- src/Pages/EditCity/index.tsx
+++ src/Pages/EditCity/index.tsx
@@ -1,28 +1,30 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { InputText } from "primereact/inputtext";
 import { InputNumber } from "primereact/inputnumber";
 // Component imports
-
 import ButtonField from "../../components/Button";
 import appConstants from "../../constants/appConstants";
+import { visulizationActions } from "../Visualization/visulizationSlice";
 
 export default function EditCity() {
   const { state }: any = useLocation();
-  let { rowData, cityList } = state;
+  // let { rowData, cityList } = state;
+  let rowData = state && state.rowData;
+  let cityList = state && state.cityList;
+
   const [currentRowData, setCurrentRowData] = useState<any>(rowData);
 
   const [tableCityList, setTableCurrentCityList] = useState<any>(cityList);
 
-  const cityName = tableCityList.filter(
-    (cityDetail: any) => cityDetail.id === currentRowData.id
-  )[0].city;
-
   const updateCityList = (newValue: string, key: string) => {
     let tempCityList = [...tableCityList];
-    const index = tableCityList.findIndex(
-      (cityDetail: any) => cityDetail.id === currentRowData.id
-    );
+    const index =
+      tableCityList &&
+      tableCityList.findIndex(
+        (cityDetail: any) => cityDetail.id === currentRowData.id
+      );
     const data = tempCityList[index];
     data[key] = newValue;
     setTableCurrentCityList(tempCityList);
@@ -32,6 +34,15 @@ export default function EditCity() {
     console.log("tableCityList", tableCityList);
   }, [tableCityList]);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const saveChanges = (e: any) => {
+    e.preventDefault();
+    dispatch(visulizationActions.getCityListSuccess(tableCityList));
+    console.log("tableCityList", tableCityList);
+    navigate("/");
+  };
+
   return (
     <div className="container">
       <h2>{appConstants.EDIT_PAGE_HEADER}</h2>
@@ -39,9 +50,11 @@ export default function EditCity() {
         <label className="mt-2">City Name</label>
         <InputText
           value={
-            tableCityList.filter(
-              (cityDetail: any) => cityDetail.id === currentRowData.id
-            )[0].city
+            (tableCityList &&
+              tableCityList.filter(
+                (cityDetail: any) => cityDetail.id === currentRowData.id
+              )[0].city) ||
+            ""
           }
           onChange={(e: any) => updateCityList(e.target.value, "city")}
           className={`d-block mb-2`}
@@ -50,9 +63,11 @@ export default function EditCity() {
         <label className="mt-2">Total Population</label>
         <InputNumber
           value={
-            tableCityList.filter(
-              (cityDetail: any) => cityDetail.id === currentRowData.id
-            )[0].totalPopulation
+            (tableCityList &&
+              tableCityList.filter(
+                (cityDetail: any) => cityDetail.id === currentRowData.id
+              )[0].totalPopulation) ||
+            0
           }
           onChange={(e: any) => updateCityList(e && e.value, "totalPopulation")}
           className={`d-block mb-2`}
@@ -61,9 +76,11 @@ export default function EditCity() {
         <label className="mt-2">Vaccinated Population</label>
         <InputNumber
           value={
-            tableCityList.filter(
-              (cityDetail: any) => cityDetail.id === currentRowData.id
-            )[0].vaccinatedPopulation
+            (tableCityList &&
+              tableCityList.filter(
+                (cityDetail: any) => cityDetail.id === currentRowData.id
+              )[0].vaccinatedPopulation) ||
+            0
           }
           onChange={(e: any) =>
             updateCityList(e && e.value, "vaccinatedPopulation")
@@ -74,9 +91,11 @@ export default function EditCity() {
         <label className="mt-2">Doses Available</label>
         <InputNumber
           value={
-            tableCityList.filter(
-              (cityDetail: any) => cityDetail.id === currentRowData.id
-            )[0].dosesAvailable
+            (tableCityList &&
+              tableCityList.filter(
+                (cityDetail: any) => cityDetail.id === currentRowData.id
+              )[0].dosesAvailable) ||
+            0
           }
           onChange={(e: any) => updateCityList(e && e.value, "dosesAvailable")}
           className={`d-block mb-2`}
@@ -85,7 +104,7 @@ export default function EditCity() {
         <ButtonField
           value="Save"
           disable={false}
-          onClickFunction={() => console.log("save")}
+          onClickFunction={(e) => saveChanges(e)}
           spacingClasses=""
         />
       </form>
